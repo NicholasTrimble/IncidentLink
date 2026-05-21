cc9b665a6866c5dc62cd1c5db1aae3bdfaaae9f6
Nicholas Trimble <nicktrimble07@gmail.com>
2026-05-21 18:19:26 -0500

    set up some controllers

---
+++ Controllers/IncidentsController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using IncidentLink.Data;
+using IncidentLink.Models;
+
+namespace IncidentLink.Controllers
+{
+    public class IncidentsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public IncidentsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Incidents
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Incidents.ToListAsync());
+        }
+
+        // GET: Incidents/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var incident = await _context.Incidents
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (incident == null)
+            {
+                return NotFound();
+            }
+
+            return View(incident);
+        }
+
+        // GET: Incidents/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Incidents/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,Description,Severity,Status,Location,CreatedAt,ResolvedAt")] Incident incident)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(incident);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(incident);
+        }
+
+        // GET: Incidents/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var incident = await _context.Incidents.FindAsync(id);
+            if (incident == null)
+            {
+                return NotFound();
+            }
+            return View(incident);
+        }
+
+        // POST: Incidents/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Title,Description,Severity,Status,Location,CreatedAt,ResolvedAt")] Incident incident)
+        {
+            if (id != incident.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(incident);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!IncidentExists(incident.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(incident);
+        }
+
+        // GET: Incidents/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var incident = await _context.Incidents
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (incident == null)
+            {
+                return NotFound();
+            }
+
+            return View(incident);
+        }
+
+        // POST: Incidents/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var incident = await _context.Incidents.FindAsync(id);
+            if (incident != null)
+            {
+                _context.Incidents.Remove(incident);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool IncidentExists(int id)
+        {
+            return _context.Incidents.Any(e => e.Id == id);
+        }
+    }
+}
